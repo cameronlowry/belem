d74fd1c770a749d0de46b960eedc1ac04a3e860d
Cameron Lowry <cameronlowry@gmail.com>
2013-04-10 02:22:56 +0200

    Google maps, image gallery & hero
    added google maps api3, resized images, tweaked image gallery 3 per row

--- prototype/js/main.js
+++ prototype/js/main.js
@@ -1,7 +1,8 @@
 $(function() {
-	$('.content .left-section.image-gallery .grid img').click( 
+	if($('.image-gallery').length > 0) {
+		$('.content .left-section.image-gallery .grid img').click( 
 		function() { 
-			$('.current-image').attr('src',this.src);
+			$('.current-image.active').attr('src',this.src);
 			$('.current-image-container').css('display','block');
 			$('.current-image-container').fadeTo(300, 1, function() {
 		      // Animation complete.
@@ -18,31 +19,106 @@ $(function() {
 
 	$('.next').click(
 		function() {
-			var src = $('.current-image').attr('src');
+			var src = $('.current-image.active').attr('src');
 			var index = src.indexOf('.jpg');
-			var character = src.substring(index - 1,index);
+			var character = src.substring(src.indexOf('s/c') + 3,index);
 			var integ = parseInt(character);
-			if(integ + 1 <= 9) {
-				$('.current-image').attr('src', src.replace(character, integ + 1));
+			console.log(character);
+			if(integ + 1 <= 12) {
+				var $active = $('.current-image.active');
+				var $next = $('.current-image.inactive');
+				$next.attr('src', src.replace(character, integ + 1));
+				$next.show().css('z-index',2);//move the next image up the pile
+				$active.fadeOut(300,function(){//fade out the top image
+					$active.css('z-index',1).hide().removeClass('active').addClass('inactive');//reset the z-index and unhide the image
+					$next.css('z-index',3).removeClass('inactive').addClass('active');//make the next image the top one
+			    });	
+
 			}
 			else {
-				$('.current-image').attr('src', src.replace(character, 1));
+				var $active = $('.current-image.active');
+				var $next = $('.current-image.inactive');
+				$next.attr('src', 'img/cakes/c1.jpg');
+				$next.show().css('z-index',2);//move the next image up the pile
+				$active.fadeOut(300,function(){//fade out the top image
+					$active.css('z-index',1).hide().removeClass('active').addClass('inactive');//reset the z-index and unhide the image
+					$next.css('z-index',3).removeClass('inactive').addClass('active');//make the next image the top one
+			    });
 			}
 
 		});
 
 	$('.prev').click(
 		function() {
-			var src = $('.current-image').attr('src');
+			var src = $('.current-image.active').attr('src');
 			var index = src.indexOf('.jpg');
-			var character = src.substring(index - 1,index);
+			var character = src.substring(src.indexOf('s/c') + 3,index);
 			var integ = parseInt(character);
 			if(integ - 1 > 0) {
-				$('.current-image').attr('src', src.replace(character, integ - 1));
+				var $active = $('.current-image.active');
+				var $next = $('.current-image.inactive');
+				$next.attr('src', src.replace(character, integ - 1));
+				$next.show().css('z-index',2);//move the next image up the pile
+				$active.fadeOut(300,function() {//fade out the top image
+					$active.css('z-index',1).hide().removeClass('active').addClass('inactive');//reset the z-index and unhide the image
+					$next.css('z-index',3).removeClass('inactive').addClass('active');//make the next image the top one
+			    });				
 			}
 			else {
-				$('.current-image').attr('src', src.replace(character, 9));
+				var $active = $('.current-image.active');
+				var $next = $('.current-image.inactive');
+				$next.attr('src', 'img/cakes/c12.jpg');
+				$next.show().css('z-index',2);//move the next image up the pile
+				$active.fadeOut(300,function(){//fade out the top image
+					$active.css('z-index',1).hide().removeClass('active').addClass('inactive');//reset the z-index and unhide the image
+					$next.css('z-index',3).removeClass('inactive').addClass('active');//make the next image the top one
+			    });
 			}
 
 		});
+	}
+
+	var map;
+	function initialize() {
+	var mapOptions = {
+	  zoom: 15,
+	  center: new google.maps.LatLng(-25.744642, 28.192829),
+	  mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	map = new google.maps.Map(document.getElementById('map-canvas'),
+	    mapOptions);
+		var styles = [
+		  {
+		    stylers: [
+		      { hue: "#F0EDE6" },
+		      { saturation: -20 }, 
+		      { lightness: +10 }
+		    ]
+		  }
+		];
+
+	map.setOptions({styles: styles});
+	TestMarker();
+	}
+
+	// Function for adding a marker to the page.
+    function addMarker(location) {
+        marker = new google.maps.Marker({
+            position: location,
+            map: map
+        });
+    }
+
+    // Testing the addMarker function
+    function TestMarker() {
+           Belem = new google.maps.LatLng(-25.744642, 28.192829);
+           addMarker(Belem);
+    }
+
+
+    if($('google').length > 0) {
+		google.maps.event.addDomListener(window, 'load', initialize);
+	}
+
+
 });
